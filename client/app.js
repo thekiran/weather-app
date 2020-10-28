7917e06836b25e9971891d3e30ce1c1f125ab30a
unknown <kirankumargs23.kkgs@gmail.com>
2020-10-28 19:18:15 +0530

    loading animation completed

---
+++ client/app.js
@@ -0,0 +1,56 @@
+const app = require('express')()
+const axios = require('axios')
+const cors = require('cors')
+
+const key = '24373ca8e3444c1a51cfd3182648c3d0'
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+// var corsOptions = {
+// 	origin: 'http://localhost:5000',
+// 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+
+app.use(cors())
+// cors(),
+app.get('/city/:cname', async (req, res) => {
+  //console.log(req.params.cname);
+  try {
+    const cname = req.params.cname
+    const base = 'http://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${cname}&appid=${key}`
+    // const key = key
+    console.log(base + query)
+    const response = await axios.get(base + query)
+    const data = await response.data
+    console.log(data)
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(400).json('serber')
+    console.log(error)
+  }
+})
+app.get('/pos/:lat/:lon', async (req, res) => {
+  //console.log(req.params.cname);
+  try {
+    const lat = req.params.lat
+    const lon = req.params.lon
+    console.log(lat + lon)
+    const base = 'http://api.openweathermap.org/data/2.5/weather'
+    const query = `?lat=${lat}&lon=${lon}&appid=${key}`
+    console.log(base + query)
+
+    const response = await axios.get(base + query)
+    const data = await response.data
+    // console.log(data);
+    // res.send('hello');
+    // res.json(response.data);
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(400).json('serber')
+    console.log(error)
+  }
+})
+
+app.listen(process.env.PORT || 5000, () => console.log('running on port 5000'))
--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,108 +1,142 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState } from 'react'
+import loader from './loading.gif'
 
-import './homepage.css';
+import './homepage.css'
 const Homepage = () => {
-	const [ cardStyle, setCardStyle ] = useState(false);
+  const [cardStyle, setCardStyle] = useState(false)
 
-	const [ info, setinfo ] = useState(null);
-	const [ formValue, setFormValue ] = useState('');
-	const [ data, setData ] = useState(false);
+  const [info, setinfo] = useState(null)
+  const [formValue, setFormValue] = useState('')
+  const [data, setData] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-	const card = () => {
-		if (cardStyle) {
-			return {
-				visibilty: 'visible',
-				transform: 'translateY(50px)',
-				transitionProperty: 'transform',
-				transitionDuration: '1s'
-			};
-		} else {
-			return { visibilty: 'visible', transform: 'translateY(300px)' };
-		}
-	};
-	const onClick = async (e) => {
-		e.preventDefault();
+  const card = () => {
+    if (cardStyle) {
+      return {
+        opacity: '1',
+        transform: 'translateY(0)',
+        transitionProperty: 'transform',
+        transitionDuration: '1s',
+      }
+    } else {
+      return { opacity: '0', transform: 'translateY(2000px)' }
+    }
+  }
+  const onClick = async (e) => {
+    e.preventDefault()
 
-		// setData(true);
-		// setCardStyle(true);
-		// console.log(e);
-		// console.log(e.target.id);
-		if (e.target.id === 'sub') {
-			const res = await fetch('http://localhost:5000/city/' + formValue);
-			if (res.status === 200) {
-				const data = await res.json();
-				// console.log(data);
-				setData(data);
-				setCardStyle(true);
-			}
-		}
-		if (e.target.id === 'coord') {
-			const coord = async (p) => {
-				const lat = p.coords.latitude;
-				const lon = p.coords.longitude;
+    // setData(true);
+    // setCardStyle(true);
+    // console.log(e);
+    // console.log(e.target.id);
+    const tar = e.target
+    const tarId = e.target.id
+    if (tar && tarId === 'sub') {
+      setLoading(true)
+      //   const res = await fetch('http://localhost:5000/city/' + formValue)
+      const res = await fetch(
+        'https://weatherapplicationn123.herokuapp.com/city/' + formValue
+      )
+      //   console.log(res)
+      if (res.status === 200) {
+        const data = await res.json()
+        // console.log(data)
+        setData(data)
+        setLoading(false)
+        setCardStyle(true)
+      }
+    }
+    if (tar && tarId === 'coord') {
+      const coord = async (p) => {
+        const lat = p.coords.latitude
+        const lon = p.coords.longitude
 
-				const response = await fetch(`http://localhost:5000/pos/${lat}/${lon}`);
+        setLoading(true)
+        const response = await fetch(
+          `https://weatherapplicationn123.herokuapp.com/pos/${lat}/${lon}`
+        )
 
-				const data = await response.json();
-				setData(data);
-				setCardStyle(true);
-			};
-			const loc = () => {
-				if (navigator.geolocation) {
-					return navigator.geolocation.getCurrentPosition(coord);
-				}
-			};
-			loc();
-		}
-		// console.log(formValue);
-	};
-	// console.log(data);
-	return (
-		<div className="main-header">
-			<div className="header-container">
-				<div className="header-content">
-					<form>
-						<input
-							className="city-inp"
-							onChange={(e) => setFormValue(e.target.value)}
-							value={formValue}
-							type="text"
-							placeholder="CITY NAME"
-						/>
-						<div className="btns">
-							<button type="submit" onClick={onClick} id="sub" className="btn inp">
-								Submit City
-							</button>
-							<button type="submit" id="coord" onClick={onClick} className="btn loc">
-								Current Location
-							</button>
-						</div>
-					</form>
-				</div>
-			</div>
-			<div className="info-container">
-				<div style={card()} className="info-content">
-					{data ? (
-						<Fragment>
-							<div className="card-body">
-								<h5 className="card-title" />
-								<h6 className="card-subtitle">{data.name}</h6>
-								<p className="card-text">
-									temparature is {data.main.temp} and max temp is {data.main.temp_max},
-								</p>
-								<p className="card-text">
-									humidity is {data.main.humidity} and pressure is{data.main.pressure}
-								</p>
-								<div className="c-link">
-									<button className="card-link">More Info</button>
-								</div>
-							</div>
-						</Fragment>
-					) : null}
-				</div>
-			</div>
-		</div>
-	);
-};
+        const data = await response.json()
+        setData(data)
+        setLoading(false)
+        setCardStyle(true)
+      }
+      const loc = () => {
+        if (navigator.geolocation) {
+          return navigator.geolocation.getCurrentPosition(coord)
+        }
+      }
+      loc()
+    }
+    // console.log(formValue);
+  }
+  // console.log(data);
+  return (
+    <div className='main-header'>
+      <div className='header-container'>
+        <div className='header-content'>
+          <form>
+            <input
+              className='city-inp'
+              onChange={(e) => setFormValue(e.target.value)}
+              value={formValue}
+              type='text'
+              placeholder='CITY NAME'
+            />
+            <div className='btns'>
+              <button
+                type='submit'
+                onClick={onClick}
+                id='sub'
+                className='btn inp'>
+                Submit City
+              </button>
+              <button
+                type='submit'
+                id='coord'
+                onClick={onClick}
+                className='btn loc'>
+                Current Location
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      {loading && (
+        <div className='load-container'>
+          <img src={loader} alt='' className='loading' />
+          {/* <h1>Loading...</h1> */}
+        </div>
+      )}
+      <div className='info-container'>
+        <div style={card()} className='info-content'>
+          {data ? (
+            <Fragment>
+              <div className='card-body'>
+                <h5 className='card-title' />
+                <h6 className='card-subtitle'>{data.name}</h6>
+                <p className='card-text'>
+                  temparature is {data.main.temp} and max temp is
+                  {data.main.temp_max},
+                </p>
+                <p className='card-text'>
+                  humidity is {data.main.humidity} and pressure is
+                  {data.main.pressure}
+                </p>
+                <div className='c-link'>
+                  <a
+                    target='_blank'
+                    href={`https://www.google.com/search?q=${data.name}+weather`}>
+                    <button className='card-link'>More Info</button>
+                  </a>
+                </div>
+              </div>
+            </Fragment>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Homepage;
+export default Homepage
